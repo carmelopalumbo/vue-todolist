c6c5f118795617775837b4d62e6cb4d13e811327
carmelopalumbo <carmelopalumbo@outlook.it>
2022-11-04 13:45:03 +0100

    add-init-vue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
 // vue
+
+// 1. avere un elenco di task da fare o fatti
+
+// 2. stamparli in pagina
+
+// 3. al click del task fare il toggle del task fatto o meno
+
+// 4. al click della croce eliminare il task se fatto, altrimenti scrivere un messaggio di errore
+
+// 5. dopo aver scritto il task con invio o al click del bottone aggiungere il nuovo task (come non fatto) in cima all elenco dei task
+
+// 6. verificare la lunghezza minima del nuovo task (min 5 caratteri)
+
+// 7. se l elenco e' vuoto non mostrare la lista ma mostrare il messagio non ci sono task
+
+const { createApp } = Vue;
+
+createApp(
+    {
+        data(){
+    
+            return{
+    
+            }
+            
+        },
+
+        methods: { 
+
+        },
+
+        mounted(){
+
+        }
+    }
+).mount('#app')
